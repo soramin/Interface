139d6e6ca54db316842be46852f4ae91c494e5e9
soramin <44796681+soramin@users.noreply.github.com>
2020-05-12 18:49:08 +0900

    Add files via upload

---
+++ IZukei.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace List6_16
+{
+    interface IZukei
+    {
+        //メソッドの定義
+        int CalcArea(int x, int y);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace List6_16
+{
+    class Triangle : IZukei
+    {
+        public int CalcArea(int x, int y)
+        {
+            throw new NotImplementedException();
+        }
+
+        static void Main(string[] args)
+        {
+            Triangle triangle = new Triangle();
+
+            int area = triangle.CalcArea(2, 3);
+        }
+    }
+}
